696131b2dba9707c40dd070b45ec2f10801e1e73
jonas <10245089@qq.com>
2025-05-20 09:49:20 +0800

    build: 配置Vite构建输出路径以优化资源管理
    在vite.config.js中添加了build配置，指定了assetsDir和rollupOptions，以便将不同类型的资源文件（如图片、CSS、JS）输出到相应的目录中，提高项目资源管理的可维护性和清晰度。

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const extType = path.extname(assetInfo.name).substring(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            return `assets/images/[name]-[hash][extname]`;
+          }
+          if (/css/i.test(extType)) {
+            return `assets/css/[name]-[hash][extname]`;
+          }
+          if (/js/i.test(extType)) {
+            return `assets/js/[name]-[hash][extname]`;
+          }
+          return `assets/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js'
+      }
+    }
+  }
 })
