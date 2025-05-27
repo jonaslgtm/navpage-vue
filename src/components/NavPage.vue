9cfe4dd6009d0ef0ddfa9c25bc6cfc624008c681
jonas <10245089@qq.com>
2025-05-27 08:43:10 +0800

    chore: 将热搜数据定时刷新间隔从30分钟调整为60分钟

--- src/components/NavPage.vue
+++ src/components/NavPage.vue
@@ -55,7 +55,7 @@ onMounted(async () => {
   // 初始化观察器
   initObserver();
   // 设置定时刷新
-  trendingInterval = setInterval(getTrendingData, 30 * 60 * 1000); // 每30分钟刷新一次
+  trendingInterval = setInterval(getTrendingData, 60 * 60 * 1000); // 每60分钟刷新一次
 });
 
 onUnmounted(() => {
